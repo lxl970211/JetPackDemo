b9f55b72404838bca3a8590a1ab6cceb18a2beba
刘小龙 <lxl970211@outlook.com>
2019-02-26 22:46:12 +0800

    1

---
+++ like/src/main/java/com/bx/like/Expansion.kt
@@ -0,0 +1,23 @@
+package com.bx.like
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import java.util.concurrent.Executors
+
+fun Context.toast(value: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, value, duration).show()
+}
+
+fun Context.jumpTo(t: Class<Any>, bundle: Bundle = Bundle()) {
+    var intent = Intent(this, t)
+    intent.putExtras(bundle)
+    startActivity(intent)
+}
+
+private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
+
+fun runOnIoThread(f: () -> Unit) {
+    IO_EXECUTOR.execute(f)
+}
--- like/src/main/java/com/bx/like/fragment/HomeFragment.kt
+++ like/src/main/java/com/bx/like/fragment/HomeFragment.kt
@@ -1,14 +1,19 @@
 package com.bx.like.fragment
 
+import android.graphics.BitmapFactory
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.UiThread
 
 import com.bx.like.R
+import com.bx.like.runOnIoThread
 import com.bx.like.viewmodel.HomeViewModel
+import kotlinx.android.synthetic.main.home_fragment.*
+import java.net.URL
 
 class HomeFragment : Fragment() {
 
@@ -28,7 +33,29 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            try {
+                runOnIoThread {
+                    var bitmap = BitmapFactory.decodeStream(URL("http://img1.gamersky.com/image2016/12/20161208_djy_248_6/gamersky_04small_08_20161281759132.jpg").openStream())
+                    activity?.runOnUiThread {
+                        imageView.setImageBitmap(bitmap)
+
+                    }
+                }
+
+
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+
+        }
+    }
+
 
 }
